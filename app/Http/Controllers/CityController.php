691f78f826e9bfad9248b705fb762fb41b88b7ac
Ariiiiiii <133253988+AriGL05@users.noreply.github.com>
2025-03-12 01:30:41 -0600

    other controllers

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    //
+    public function index()
+    {
+        $city = City::all();
+        return response()->json($city);
+    }
+
 }
