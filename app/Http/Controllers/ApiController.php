318c4efa0cb97a7fbd72144c46c9afca2bf8b071
Ariiiiiii <133253988+AriGL05@users.noreply.github.com>
2025-03-13 10:25:06 -0600

    Get cities

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\City;
 use App\Models\Language;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -26,5 +27,10 @@ class ApiController extends Controller
     {
         $categories = Category::all();
         return response()->json($categories);
+    }
+    public function getCities()
+    {
+        $cities = City::all();
+        return response()->json($cities);
     }
 }
